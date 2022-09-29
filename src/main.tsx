e1df40ca8e59cf3dece48cc0d2916766118a38c0
lashdid19 <lashdid19@gmail.com>
2022-09-29 12:38:26 +0700

    feat: implement all components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import "@fontsource/poppins"
 import './index.scss'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import Register from './pages/register'
+import Login from './pages/login'
+import Profile from './pages/profile'
+import Chat from './pages/chat'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to='/register'/>,
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  },
+  {
+    path: '/chat',
+    element: <Chat />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
