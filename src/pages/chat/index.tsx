28a81f27170e310e5b999f1248b1c9316cceb43a
lashdid <lashdid19@gmail.com>
2022-10-06 16:09:59 +0700

    feat: implement chat components in pages

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,41 +1,42 @@
-import InputChat from "@components/Chat/InputChat";
 import styles from "./styles.module.scss";
+import "@fontsource/poppins/600.css";
+import { useEffect, useRef, useState } from "react";
+import {
+  ChatContainer,
+  FullInputChat,
+  Navbar,
+  Sidebar,
+} from "@components/Chat";
+
+export type dataProps = {
+  profileUrl: string;
+  name: string;
+  sendAt: string;
+  message: string;
+};
 
 export default function Chat() {
+  const [data, setData] = useState<dataProps[]>([]);
+
   return (
     <main className={styles.main}>
-      <aside className={styles.sidebar}>
-        <h3>Chad's Room</h3>
-        <small>Humans (3/10)</small>
-        <div className={styles.humans_container}>
-          <div className={styles.human}>
-            <img src="assets/gigachad_background.jpg" alt="" />
-            <span>DeadFace69</span>
-          </div>
-          <div className={styles.human}>
-            <img src="assets/gigachad.png" alt="" />
-            <span>Chad Man</span>
-          </div>
-          <div className={styles.human}>
-            <img src="assets/gigachad1.png" alt="" />
-            <span>The Chaddest</span>
-          </div>
-        </div>
-      </aside>
+      <Sidebar />
       <div className={styles.content}>
-        <nav className={styles.navbar}>
-          <span>#123456</span>
-          <a href="/profile">Leave Room</a>
-        </nav>
-        <div className={styles.chat_container}>
-
-        </div>
-        <div className={styles.input_container}>
-          <InputChat placeholder='Type a message' multiline/>
-          <div className={styles.button_container}>
-            <button>Send</button>
-          </div>
-        </div>
+        <Navbar />
+        <ChatContainer data={data} />
+        <FullInputChat
+          submitData={(inputValue) =>
+            setData([
+              ...data,
+              {
+                profileUrl: "assets/gigachad_background.jpg",
+                name: "DeadFace69",
+                sendAt: "4:20 PM",
+                message: inputValue,
+              },
+            ])
+          }
+        />
       </div>
     </main>
   );
